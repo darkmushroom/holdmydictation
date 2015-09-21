f3ced5c589b48a2f2b4bc02ab73198144db35361
Darkmushroom <mensing.alexander@gmail.com>
2015-09-21 17:32:44 -0400

    Added general test file and initial program

---
+++ holdmydictation.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+def main():
+	root.title("HMD Main")
+	ttk.Button(root, text="Just a button").grid()
+	root.mainloop()
+
+if __name__ == '__main__':
+	main()
---
+++ threeshakes.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestHMDict(unittest.TestCase):
+
+	def test_setUp(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
