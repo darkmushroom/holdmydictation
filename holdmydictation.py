262f795a3e06b13a008d2677b8c34c3c74288efc
Darkmushroom <mensing.alexander@gmail.com>
2015-09-24 18:55:25 -0400

    Text box added. Fills entire window

--- holdmydictation.py
+++ holdmydictation.py
@@ -1,10 +1,24 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-def main():
-	root.title("HMD Main")
-	ttk.Button(root, text="Just a button").grid()
-	root.mainloop()
+import tkinter as tk
+from tkinter import Text
 
-if __name__ == '__main__':
-	main()
+#indicates we want a widget to expand in all directions 
+inflate="swne"
+
+class HMDmain(tk.Frame):
+    def __init__(self,parent, *args, **kwargs):
+        tk.Frame.__init__(self,parent, *args, **kwargs)
+        
+        
+
+        #define UI elements
+        self.parent = parent
+        self.text = Text(self)
+
+        #show UI elements by invoking the UI manager on them
+        self.text.grid(sticky=inflate)
+        self.grid(sticky=inflate)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    HMDmain(root).grid(sticky=inflate)
+    root.mainloop()
