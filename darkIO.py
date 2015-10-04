21ce1326cdd3d29ff1f1feadc9333dc9b67b6734
Darkmushroom <mensing.alexander@gmail.com>
2015-10-03 21:57:58 -0400

    file read and write class

--- darkIO.py
+++ darkIO.py
@@ -1,5 +1,14 @@
 class DarkIO():
-    def save(fname,text):
-        f = open(fname,'w')
-        f.write(text)
-        f.close()
+    """Wraps file reading and writing
+
+    These could be called from HMDmain but we will want encryption here later
+    on. Best to separate this utility now.
+    """
+
+    def save(fname, text):
+        with open(fname, 'w') as f:
+            f.write(text)
+
+    def load(fname):
+        with open(fname, 'r') as f:
+            return f.read()
--- threeshakes.py
+++ threeshakes.py
@@ -2,13 +2,21 @@ import unittest
 import darkIO
 import os.path
 
-class TestHMDict(unittest.TestCase):
 
+class TestHMDict(unittest.TestCase):
     def test_saves_file_by_name(self):
-        darkIO.DarkIO.save("filename.txt","This is some test text")
-        print(os.path)
-        self.assertTrue(os.path.isfile("filename.txt"))        
+        darkIO.DarkIO.save("filename.txt", "This is some test text")
+        self.assertTrue(os.path.isfile("filename.txt"))
+
+    def test_loads_file_by_name(self):
+        darkIO.DarkIO.save("filename.txt", "This is some test text")
+        self.assertEqual(darkIO.DarkIO.load("filename.txt"),
+                         "This is some test text")
+
+    def tearDown(self):
+
         os.remove("filename.txt")
 
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
